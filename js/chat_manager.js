190428ec3ccff1d57335049457d3b9e41f0ebf7d
Mason Prey <masonprey7@gmail.com>
2016-10-28 12:53:20 -0500

    finish emoticon modal with javascript correspondence

--- js/chat_manager.js
+++ js/chat_manager.js
@@ -1,7 +1,32 @@
 $(function() {
 
-  var chat_manager = {
+  var $chat_textarea = $('#chat-textarea');
+  var $chat_online = $('#chat-online');
+  var $chat_submit = $('#chat-submit');
+  var $chat_emote = $('#emote-wrapper a');
+
+  var chat_manager = new ChatManager();
+
+  function ChatManager() {
+    //TODO instantiate?
+  }
+
+  ChatManager.prototype.addNotification = function() {
 
   };
+
+  ChatManager.prototype.clearNotifications = function() {
+
+  };
+
+  ChatManager.prototype.appendText = function(text) {
+    var newText = $chat_textarea.val() + text;
+    $chat_textarea.val(newText);
+  };
+
+  $chat_emote.on('click', function(event) {
+    event.preventDefault();
+    chat_manager.appendText($(this).children('span').text());
+  });
 
 });
